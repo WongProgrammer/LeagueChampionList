3dc1547df7879b21fa80d0b43dbd0bffbc870dbf
WongProgrammer <francisnguyen2344@gmail.com>
2022-01-18 21:55:24 -0800

    added search button

--- src/App.js
+++ src/App.js
@@ -5,32 +5,47 @@ import React, { useState } from 'react';
 function App() {
   const [champions, setChampions] = useState(data);
   const [isClear, setIsClear] = useState(false);
+  const [search, setSearch] = useState('');
 
- function onClickHandler() {
-    if(isClear) {
+  function onClickHandler() {
+    if (isClear) {
       setChampions(data);
     } else {
       setChampions([]);
     }
     setIsClear(!isClear);
   };
+
+  function onChangeHandler(e) {
+    e.preventDefault();
+    setSearch(e.target.value);
+   // champions.filter(champion => champion.name.toLowerCase().includes(search.toLowerCase()));
+  }
+
   return (
     <div>
-      <div id="top"/>
-      <a href="#bot"><img className="pageControls" src="https://raw.githubusercontent.com/esports-bits/lol_images/master/role_lane_icons/ADC.png" alt="bottom lane icon"/></a>
-<div className="App">
+      <div id="top" />
+      <input className="search"type="text" onChange={onChangeHandler} placeholder="Search Champion" />
+      <a href="#bot">
+        <img className="pageControls" src="https://raw.githubusercontent.com/esports-bits/lol_images/master/role_lane_icons/ADC.png" alt="bottom lane icon" />
+      </a>
       <h1>League Champions</h1>
-      <List
-      className = "list"
-        champions ={champions}
-      />
-      <button onClick={onClickHandler}>{(isClear)? 'Install League' : 'Uninstall League'}</button>
-      
+      <div className="App">
+        
+        <List
+          className="list"
+          champions={champions.filter(champion => champion.name.toLowerCase().includes(search.toLowerCase()))}
+        />
+        
+
+      </div>
+      <a href="#top">
+        <img className="pageControls" src="https://raw.githubusercontent.com/esports-bits/lol_images/master/role_lane_icons/TOP.png" alt="top lane icon" />
+        </a>
+        <button onClick={onClickHandler}>{(isClear) ? 'Install League' : 'Uninstall League'}</button>
+      <div id="bot" />
     </div>
-    <a href="#top"><img className="pageControls" src="https://raw.githubusercontent.com/esports-bits/lol_images/master/role_lane_icons/TOP.png" alt="top lane icon"/></a>
-      <div id="bot"/>
-    </div>
-    
+
   );
 }
 
